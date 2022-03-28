0d02aa71d3a8b2533e1acb517c4c46774854f3f2
Rigo Carbajal <rigo.carbajal@unfinished.com>
2022-03-28 16:40:06 -0700

    Adding unit tests to DSNPWalletExample.

--- Tests/DSNPWalletTests/DSNPWalletTests.swift
+++ Tests/DSNPWalletTests/DSNPWalletTests.swift
@@ -1,6 +1,19 @@
+//
+//  DSNPWalletTests.swift
+//  DSNPWalletTests
+//
+//  Created by Rigo Carbajal on 3/28/22.
+//
+
 import XCTest
 @testable import DSNPWallet
 
-final class DSNPWalletTests: XCTestCase {
+class DSNPWalletTests: XCTestCase {
 
+    // DSNPWallet tests have been added to the example project @
+    // ~/Example/DSNPWalletExample/DSNPWalletExampleTests/DSNPWalletExampleTests.swift
+    // The reason for this is that key generation in the Secure Enclave seems to
+    // require a host application in order to function properly. Will leave this as
+    // a future item to determine a better way to maintain all unit tests under the
+    // same workflow.
 }
